d464807fff3f62b622d98a870f9a12db73e0d7c2
Pixoll <pixoll.gamer@gmail.com>
2024-08-17 20:41:41 -0400

    Basic nonogram game class

---
+++ src/core/game.py
@@ -0,0 +1,68 @@
+type rgb_t = tuple[int, int, int]
+type nonogram_t = list[list[rgb_t | None]]
+
+
+class Hint:
+    value: int
+    color: rgb_t
+
+    def __init__(self, color: rgb_t):
+        self.value = 1
+        self.color = color
+
+    def __repr__(self):
+        return f'Hint({self.value}, {self.color})'
+
+
+class Game:
+    original_nonogram: nonogram_t
+    used_colors: list[rgb_t]
+    horizontal_hints: list[list[Hint]]
+    vertical_hints: list[list[Hint]]
+
+    def __init__(self, nonogram: nonogram_t):
+        self.original_nonogram = nonogram
+        self.horizontal_hints = [Game._get_hints(row) for row in nonogram]
+        self.vertical_hints = [Game._get_hints(column) for column in list(zip(*nonogram))]
+        self.used_colors = []
+
+        for row in nonogram:
+            for color in row:
+                if color is not None and color not in self.used_colors:
+                    self.used_colors.append(color)
+
+    @staticmethod
+    def _get_hints(row_or_column: list[rgb_t]) -> list[Hint]:
+        hints: list[Hint] = []
+        skipped: bool = False
+
+        for color in row_or_column:
+            if color is None:
+                skipped = True
+                continue
+
+            if len(hints) == 0:
+                hints.append(Hint(color))
+                skipped = False
+                continue
+
+            if hints[-1].color == color and not skipped:
+                hints[-1].value += 1
+            else:
+                hints.append(Hint(color))
+
+            skipped = False
+
+        return hints
+
+
+r = (255, 0, 0)
+g = (0, 255, 0)
+b = (0, 0, 255)
+n = None
+
+game = Game([
+    [n, b, r],
+    [n, g, n],
+    [b, r, n],
+])
