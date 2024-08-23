9cc1d1f0a3c38ab2b221e13789aefc77c465e4ac
Pixoll <pixoll.gamer@gmail.com>
2024-08-23 00:49:38 -0400

    Add player nonogram

--- src/core/game.py
+++ src/core/game.py
@@ -1,27 +1,29 @@
 type rgb_t = tuple[int, int, int]
-type nonogram_t = list[list[rgb_t | None]]
-
-
-class Hint:
-    value: int
-    color: rgb_t
-
-    def __init__(self, color: rgb_t):
-        self.value = 1
-        self.color = color
-
-    def __repr__(self):
-        return f"Hint({self.value}, {self.color})  "
 
 
 class Game:
-    original_nonogram: nonogram_t
+    class Hint:
+        value: int
+        color: rgb_t
+
+        def __init__(self, color: rgb_t):
+            self.value = 1
+            self.color = color
+
+        def __repr__(self):
+            return f"Hint({self.value}, {self.color})  "
+
+    CROSS = 0
+
+    _original_nonogram: list[list[rgb_t | None]]
+    player_nonogram: list[list[rgb_t | CROSS | None]]
     used_colors: list[rgb_t]
     horizontal_hints: list[list[Hint]]
     vertical_hints: list[list[Hint]]
 
-    def __init__(self, nonogram: nonogram_t):
-        self.original_nonogram = nonogram
+    def __init__(self, nonogram: list[list[rgb_t | None]]):
+        self._original_nonogram = nonogram
+        self.player_nonogram = [[None for _ in range(len(nonogram[0]))] for _ in range(len(nonogram))]
         self.horizontal_hints = [Game._get_hints(row) for row in nonogram]
         self.vertical_hints = [Game._get_hints(column) for column in list(zip(*nonogram))]
         self.used_colors = []
@@ -33,7 +35,7 @@ class Game:
 
     @staticmethod
     def _get_hints(row_or_column: list[rgb_t]) -> list[Hint]:
-        hints: list[Hint] = []
+        hints: list[Game.Hint] = []
         skipped: bool = False
 
         for color in row_or_column:
@@ -42,14 +44,14 @@ class Game:
                 continue
 
             if len(hints) == 0:
-                hints.append(Hint(color))
+                hints.append(Game.Hint(color))
                 skipped = False
                 continue
 
             if hints[-1].color == color and not skipped:
                 hints[-1].value += 1
             else:
-                hints.append(Hint(color))
+                hints.append(Game.Hint(color))
 
             skipped = False
 
