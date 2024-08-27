d01cfc200901c39bc996259c96efa4b98b7c20b4
ak0mak0 <igpadilla2021@udec.cl>
2024-08-26 22:17:26 -0400

    alignment was added in row and column

--- src/components/column.py
+++ src/components/column.py
@@ -8,6 +8,9 @@ class Column(Element):
         self.__update_size()
         self.separation = 0
 
+        self.maxWidth = 0
+        self.align = "center"
+
     """OPCIONES DE COLUMN"""
     def add_child(self, element: Element):
         self.elements.append(element)
@@ -29,6 +32,12 @@ class Column(Element):
     def get_childs(self) -> list[Element]:
         return self.elements
 
+    def alignment(self, alignment: str):
+        if alignment in [
+            "center", "left", "right"
+        ]:
+            self.align = alignment
+            self.__update_positions()
 
     """OPCIONES DE RENDER"""
     def render(self, window):
@@ -43,11 +52,18 @@ class Column(Element):
             self.height = 0
             return
         self.width = max(el.width for el in self.elements)
+        self.maxWidth = self.width
         self.height = sum(el.height for el in self.elements) + self.separation*(len(self.elements)-1)
 
     def __update_positions(self):
         current_y = 0
         for element in self.elements:
-            element_position = (self.position[0], self.position[1] + current_y)
+            if self.align == "center":
+                align = (self.maxWidth - element.get_size()[0]) / 2
+            elif self.align == "left":
+                align = 0
+            else:
+                align = (self.maxWidth - element.get_size()[0])
+            element_position = (self.position[0] +  align, self.position[1] + current_y)
             element.set_position(element_position)
             current_y += element.height + self.separation
--- src/components/row.py
+++ src/components/row.py
@@ -8,6 +8,9 @@ class Row(Element):
         self.__update_size()
         self.separation = 0
 
+        self.maxHeigth = 0
+        self.align = "center"
+
     """OPCIONES DE ROW"""
     def add_child(self, element: Element):
         self.elements.append(element)
@@ -29,6 +32,12 @@ class Row(Element):
     def get_childs(self) -> list[Element]:
         return self.elements
 
+    def alignment(self, alignment: str):
+        if alignment in [
+            "center", "top", "bottom"
+        ]:
+            self.align = alignment
+            self.__update_positions()
 
     """OPCIONES DE RENDER"""
     def render(self, window):
@@ -44,10 +53,17 @@ class Row(Element):
             return
         self.width = sum(el.width for el in self.elements) + self.separation*(len(self.elements)-1)
         self.height = max(el.height for el in self.elements)
+        self.maxHeigth = self.height
 
     def __update_positions(self):
         current_x = 0
         for element in self.elements:
-            element_position = (self.position[0] + current_x, self.position[1])
+            if self.align == "center":
+                align = (self.maxHeigth - element.get_size()[1]) / 2
+            elif self.align == "top":
+                align = 0
+            else:
+                align = (self.maxHeigth - element.get_size()[1])
+            element_position = (self.position[0] + current_x, self.position[1] + align)
             element.set_position(element_position)
             current_x += element.width + self.separation
