4db0bf0b8817b66da35d3641116b3c46fb0f34dd
ak0mak0 <akmak2470@gmail.com>
2024-08-27 01:36:05 -0400

    border can be invisible

--- src/components/container.py
+++ src/components/container.py
@@ -2,17 +2,16 @@ import pygame
 from components import Area
 from components import Element
 
-
 class Container(Element):
     def __init__(self, size: tuple[int, int]):
         super().__init__(size[0], size[1])
         self.size = size
         self.surface = pygame.Surface(size, pygame.SRCALPHA)
         self.background_color = (255, 255, 255, 0)
-        self.border_color = (255, 255, 255)
+        self.border_color = (255, 255, 255, 255)
 
         self.surface.fill(self.background_color)
-        pygame.draw.rect(self.surface, self.border_color, self.surface.get_rect(), 1)
+        self._draw_border()
 
         self.position = (0, 0)
         self.child = None
@@ -21,16 +20,15 @@ class Container(Element):
         self.align = "center"
         self.image = None
 
-
     """OPCIONES DE CONTAINER"""
     def set_color(self, new_color: tuple[int, int, int, int]):
         self.background_color = new_color
         self.surface.fill(self.background_color)
-        pygame.draw.rect(self.surface, self.border_color, self.surface.get_rect(), 1)
+        self._draw_border()
 
-    def set_border(self, border_color: tuple[int, int, int]):
+    def set_border(self, border_color: tuple[int, int, int, int]):
         self.border_color = border_color
-        pygame.draw.rect(self.surface, self.border_color, self.surface.get_rect(), 1)
+        self._draw_border()
 
     def set_position(self, new_position: tuple[int, int]):
         self.position = new_position
@@ -57,7 +55,6 @@ class Container(Element):
             self.align = alignment
             self.__update_position()
 
-
     """OPCIONES DE RENDER"""
     def render(self, window: pygame.Surface):
         if self.image:
@@ -66,8 +63,10 @@ class Container(Element):
         if self.child:
             self.child.render(window)
 
-
     """FUNCIONES COMPLEMENTARIA"""
+    def _draw_border(self):
+        pygame.draw.rect(self.surface, self.border_color, self.surface.get_rect(), 1)
+
     def __update_position(self):
         if self.child:
             obj_size = self.child.get_size()
