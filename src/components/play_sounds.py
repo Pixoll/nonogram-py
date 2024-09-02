c8474e29f17ca6cda5a876aa3e7b3fb2ad622320
Roberto <pantostado1601@gmail.com>
2024-09-02 12:10:05 -0400

    Added more sound Effects and their functions

--- src/components/play_sounds.py
+++ src/components/play_sounds.py
@@ -6,3 +6,16 @@ def play_music():
     pygame.mixer.music.load("assets/Music/Music01.mp3")
     pygame.mixer.music.set_volume(0.3)
     pygame.mixer.music.play(-1)
+
+def play_MenuClickSoundEffect():
+    pygame.mixer.init()
+    sound = pygame.mixer.Sound("assets\Music\MenuClickSound.mp3")
+    sound.set_volume(0.3)
+    sound.play()
+
+def play_GameClickEffect():
+    pygame.mixer.init()
+    sound = pygame.mixer.Sound("assets\Music\ClickSound.mp3")
+    sound.set_volume(0.3)
+    sound.play()
+
