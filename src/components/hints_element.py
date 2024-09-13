c04213f32639a17e957ea5f371cfdf4ef39ff200
Pixoll <pixoll.gamer@gmail.com>
2024-09-13 02:21:09 -0300

    Fix position and size calculations

--- src/components/hints_element.py
+++ src/components/hints_element.py
@@ -15,48 +15,37 @@ class HintsElement(Element):
     _padding: int
     _nonogram: Nonogram
     _hints: tuple[tuple[Nonogram.Hint, ...], ...]
-    _is_horizontal: bool
 
-    def __init__(self, size: int, nonogram: Nonogram, padding: int, is_horizontal: bool):
-        super().__init__(size, size)
-        self._nonogram = nonogram
+    def __init__(
+            self,
+            hints: tuple[tuple[Nonogram.Hint, ...], ...],
+            block_size: int,
+            padding: int,
+            is_horizontal: bool
+    ):
+        super().__init__(0, 0)
+        self._hints = hints
         self._padding = padding
-        self._is_horizontal = is_horizontal
-        hints_font = font.SysFont("Arial", int(size / 1.5))
+        hints_font = font.SysFont("Arial", int(block_size / 1.5))
 
-        if self._is_horizontal:
-            self._hints = nonogram.horizontal_hints
-            self._colum_or_row = Column()
-            for i in range(len(self._hints)):
-                row = Row()
+        self._hint_elements = Column() if is_horizontal else Row()
+        for i in range(len(hints)):
+            row_or_column = Row() if is_horizontal else Column()
 
-                for hint in self._hints[i]:
-                    hint_block = ColoredBlock(size, hint.color, str(hint.value), hints_font)
-                    row.add_element(hint_block)
+            for hint in hints[i]:
+                hint_block = ColoredBlock(block_size, hint.color, str(hint.value), hints_font)
+                row_or_column.add_element(hint_block)
 
-                row.set_padding(padding)
-                self._colum_or_row.add_element(row)
-                self._colum_or_row.set_alignment(HorizontalAlignment.RIGHT)
-            self._colum_or_row.set_padding(padding)
-        else:
-            self._hints = nonogram.vertical_hints
-            self._colum_or_row = Row()
+            row_or_column.set_padding(padding)
+            self._hint_elements.add_element(row_or_column)
+            self._hint_elements.set_alignment(HorizontalAlignment.RIGHT if is_horizontal else VerticalAlignment.BOTTOM)
 
-            for i in range(len(self._hints)):
-                column = Column()
-
-                for hint in self._hints[i]:
-                    hint_block = ColoredBlock(size, hint.color, str(hint.value), hints_font)
-                    column.add_element(hint_block)
-
-                column.set_padding(padding)
-                self._colum_or_row.add_element(column)
-                self._colum_or_row.set_alignment(VerticalAlignment.BOTTOM)
-            self._colum_or_row.set_padding(padding)
+        self._hint_elements.set_padding(padding)
+        self._width, self._height = self._hint_elements.size
 
     def set_position(self, position: tuple[int, int]) -> Self:
         self._position = position
-        self._colum_or_row.set_position(position)
+        self._hint_elements.set_position(position)
         return self
 
     @property
@@ -67,4 +56,4 @@ class HintsElement(Element):
         pass
 
     def render(self, window: Surface) -> None:
-        self._colum_or_row.render(window)
+        self._hint_elements.render(window)
--- src/components/nonogram_element.py
+++ src/components/nonogram_element.py
@@ -21,54 +21,52 @@ class NonogramElement(Element):
     _vertical_hints: HintsElement
     _selected_color: tuple[int, int, int]
 
-    def __init__(self, nonogram: Nonogram, size: int, padding: int):
-        super().__init__(nonogram.size[0] * (size + padding) + padding, nonogram.size[1] * (size + padding) + padding)
+    def __init__(self, nonogram: Nonogram, block_size: int, padding: int):
+        super().__init__(
+            nonogram.size[0] * (block_size + padding) + padding,
+            nonogram.size[1] * (block_size + padding) + padding
+        )
 
         self._background_color = (192, 192, 192)
         self._padding = padding
-        self._row = Row()
-        self._block_size = size
-        self._horizontal_hints = HintsElement(size, nonogram, padding, True)
-        self._vertical_hints = HintsElement(size, nonogram, padding, False)
+        self._grid = Row()
+        self._block_size = block_size
+        self._horizontal_hints = HintsElement(nonogram.horizontal_hints, block_size, padding, True)
+        self._vertical_hints = HintsElement(nonogram.vertical_hints, block_size, padding, False)
         self._selected_color = nonogram.used_colors[0]
 
         for i in range(nonogram.size[0]):
             column = Column()
             for j in range(nonogram.size[1]):
-                column.add_element(Block(size, size, nonogram[i, j]))
-            self._row.add_element(column)
+                column.add_element(Block(block_size, block_size, nonogram[i, j]))
+            self._grid.add_element(column)
             column.set_padding(padding)
 
-        self._row.set_padding(padding)
+        self._grid.set_padding(padding)
 
         self._surface = Surface((self.size[0] + padding * 2, self.size[1] + padding * 2), pygame.SRCALPHA)
         self._surface.fill(self._background_color)
 
     def set_position(self, position: tuple[int, int]) -> Self:
         self._position = position
-        max_horizontal_hint_blocks = max(len(hints) for hints in self._horizontal_hints.hints)
-        max_vertical_hint_blocks = max(len(hints) for hints in self._vertical_hints.hints)
-
         vertical_hint_position = (
-            self._position[0],
-            self._position[1] - max_vertical_hint_blocks * (self._padding + self._block_size)
+            position[0],
+            position[1] - self._vertical_hints.size[1] - self._padding * 2
         )
-
         horizontal_hint_position = (
-            self._position[0] - max_horizontal_hint_blocks * (self._padding + self._block_size),
-            self._position[1]
+            position[0] - self._horizontal_hints.size[0] - self._padding * 2,
+            position[1]
         )
 
         self._vertical_hints.set_position(vertical_hint_position)
         self._horizontal_hints.set_position(horizontal_hint_position)
-
-        self._row.set_position(self._position)
+        self._grid.set_position(position)
 
         return self
 
     @property
     def size(self) -> tuple[int, int]:
-        return self._row.size
+        return self._grid.size
 
     def set_background_color(self, color: tuple[int, int, int] | tuple[int, int, int, int]) -> Self:
         self._background_color = color
@@ -81,12 +79,9 @@ class NonogramElement(Element):
 
     def render(self, window: Surface):
         window.blit(self._surface, (self.position[0] - self._padding, self._position[1] - self._padding))
-
         self._vertical_hints.render(window)
-
         self._horizontal_hints.render(window)
-
-        self._row.render(window)
+        self._grid.render(window)
 
     def on_all_events(self, event: Event) -> None:
         if event.type != EventType.MOUSE_BUTTON_DOWN:
@@ -95,7 +90,7 @@ class NonogramElement(Element):
         if event.button != MouseButton.LEFT and event.button != MouseButton.RIGHT:
             return
 
-        for column in self._row.elements:
+        for column in self._grid.elements:
             for block in column.elements:
                 # noinspection PyTypeChecker
                 b: Block = block
