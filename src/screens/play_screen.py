2edd3b1fe4abb89a1c467e6ca405890a13ef4780
Pixoll <pixoll.gamer@gmail.com>
2024-09-13 17:17:36 -0300

    Display message when nonogram is completed

--- src/screens/play_screen.py
+++ src/screens/play_screen.py
@@ -1,4 +1,5 @@
 import pygame
+from pygame import font, Surface
 
 from components import ChildAlignment, ColorPicker, Container, NonogramElement
 from core.nonogram import Nonogram
@@ -10,17 +11,22 @@ from screens.screen import Screen
 class PlayScreen(Screen):
     _engine: Engine
     _menu: Container
-    _nonogram: NonogramElement
+    _nonogram: Nonogram
+    _nonogram_element: NonogramElement
     _color_picker: ColorPicker
+    _completed_text: Surface
+
     def __init__(self, engine: Engine, nonogram: Nonogram):
         self._engine = engine
         self._menu = Container(1280, 720).set_child_alignment(ChildAlignment.CENTER)
-        self._nonogram = NonogramElement(nonogram, 25, 1)
-        self._color_picker = ColorPicker(self._nonogram, nonogram.used_colors, 50, 1)
-        self._menu.set_child(self._nonogram)
+        self._nonogram = nonogram
+        self._nonogram_element = NonogramElement(nonogram, 25, 1)
+        self._color_picker = ColorPicker(self._nonogram_element, nonogram.used_colors, 50, 1)
+        self._menu.set_child(self._nonogram_element)
+        self._completed_text = font.SysFont("Arial", 30).render("completed!", True, (0, 0, 0))
 
     def on_all_events(self, event: Event) -> None:
-        self._nonogram.on_all_events(event)
+        self._nonogram_element.on_all_events(event)
         self._color_picker.on_all_events(event)
 
     def on_key_event(self, key_event: KeyEvent) -> None:
@@ -39,3 +45,6 @@ class PlayScreen(Screen):
         window = pygame.display.get_surface()
         self._menu.render(window)
         self._color_picker.render(window)
+
+        if self._nonogram.is_completed:
+            window.blit(self._completed_text, (0, 0))
