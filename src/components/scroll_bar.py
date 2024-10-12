e875129b8532811c10a199d8b9590448053d9e44
benjag22 <benjagonzalez2022@udec.cl>
2024-10-12 13:52:55 -0300

    refactoring ScrollBar

--- src/components/scroll_bar.py
+++ src/components/scroll_bar.py
@@ -2,13 +2,14 @@ import pygame
 from components import Element
 from components.block_with_image import BlockWithImage
 from typing import Self
-from events import Event, EventType, MouseButton, MouseWheelEvent
+from events import Event, EventType, MouseButton
 
 GRAY = (197, 194, 197)
 
 class ScrollBar(Element):
     _bar_left: BlockWithImage
     _bar_right: BlockWithImage
+    _bar: BlockWithImage
 
     def __init__(self, width: int, content_width: int):
         super().__init__(width, 20)
@@ -16,11 +17,11 @@ class ScrollBar(Element):
         self.content_width = content_width
         self.change_x = 0
 
-        bar_width = int((width - 40) / (content_width / (width * 1.0)))
-        self.bar_rect = pygame.Rect(20, self._position[1], bar_width, 20)
+        bar_width = int((width - 40) * (width / content_width))
 
         self._bar_left = BlockWithImage(20, 20, "left.png")
         self._bar_right = BlockWithImage(20, 20, "right.png")
+        self._bar = BlockWithImage(bar_width, 20, "bar.png")
 
         self.on_bar = False
         self.mouse_diff = 0
@@ -29,6 +30,7 @@ class ScrollBar(Element):
         self._position = position
         self._bar_left.set_position((position[0], position[1]))
         self._bar_right.set_position((position[0] + self._width - 20, position[1]))
+        self._bar.set_position((position[0] + 20, position[1]))
         return self
 
     def update(self):
@@ -40,38 +42,31 @@ class ScrollBar(Element):
             self.x_axis = self._width - self.content_width
 
         width_diff = self.content_width - self._width
-        scroll_length = self._width - self.bar_rect.width - 40
-        bar_half_length = self.bar_rect.width / 2 + 20
+        scroll_length = self._width - self._bar.size[0] - 40
+        bar_half_length = self._bar.size[0] / 2 + 20
 
         if self.on_bar:
             pos = pygame.mouse.get_pos()
-            self.bar_rect.x = pos[0] - self.mouse_diff
-            if self.bar_rect.left < 20:
-                self.bar_rect.left = 20
-            elif self.bar_rect.right > (self._width - 20):
-                self.bar_rect.right = self._width - 20
+            self._bar.set_position((pos[0] - self.mouse_diff, self._position[1]))
 
-            self.x_axis = int(width_diff / (scroll_length * 1.0) * (self.bar_rect.centerx - bar_half_length) * -1)
-        else:
-            self.bar_rect.centerx = scroll_length / (width_diff * 1.0) * (self.x_axis * -1) + bar_half_length
+            if self._bar.position[0] < 20:
+                self._bar.set_position((20, self._position[1]))
+            elif self._bar.position[0] + self._bar.size[0] > self._width - 20:
+                self._bar.set_position((self._width - self._bar.size[0] - 20, self._position[1]))
+
+            self.x_axis = int(width_diff / scroll_length * (self._bar.position[0] - bar_half_length) * -1)
 
     def on_all_events(self, event: Event) -> None:
-
-        if event.type == EventType.MOUSE_WHEEL:
-            wheel_event: MouseWheelEvent = event
-            down = wheel_event.precise_y < 0 if wheel_event.flipped else wheel_event.precise_y > 0
-            self.change_x = -5 if down else 5
-
         pos = pygame.mouse.get_pos()
 
         if event.type == EventType.MOUSE_BUTTON_DOWN and event.button == MouseButton.LEFT:
-            pos = pygame.mouse.get_pos()
             if self._bar_left.contains(pos):
-                print("Presionó barra izquierda")
                 self.change_x = 5
             elif self._bar_right.contains(pos):
-                print("Presionó barra derecha")
                 self.change_x = -5
+            elif self._bar.contains(pos):
+                self.on_bar = True
+                self.mouse_diff = pos[0] - self._bar.position[0]
 
         if event.type == EventType.MOUSE_BUTTON_UP and event.button == MouseButton.LEFT:
             self.change_x = 0
@@ -87,9 +82,9 @@ class ScrollBar(Element):
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                 self.change_x = 0
 
-
     def render(self, window: pygame.Surface) -> None:
-        pygame.draw.rect(window, GRAY, self.bar_rect)
+        self.update()
+        self._bar.render(window)
         self._bar_left.render(window)
         self._bar_right.render(window)
 
