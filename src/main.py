c00a840a8694fdc10f1eb8fb0d4e6ba096e7adfa
Pixoll <pixoll.gamer@gmail.com>
2024-10-31 13:53:41 -0300

    Maximize window on startup

--- src/main.py
+++ src/main.py
@@ -1,13 +1,16 @@
 import pygame
+from pygame._sdl2 import Window
 
+from components import Sound
 from engine import Engine
 from screens.main_menu_screen import MainMenuScreen
-from components import Sound
 
 pygame.init()
 
 clock = pygame.time.Clock()
 window = pygame.display.set_mode((1280, 720), flags=pygame.RESIZABLE)
+
+Window.from_display_module().maximize()
 
 pygame.display.set_caption("Nonogram")
 
