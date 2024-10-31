b6a5ee8ccb940c89891c39ea4803c1e6d7188cf9
Pixoll <pixoll.gamer@gmail.com>
2024-10-31 13:32:58 -0300

    Implement exit button logic

--- src/screens/main_menu_screen.py
+++ src/screens/main_menu_screen.py
@@ -2,7 +2,7 @@ import pygame
 
 from components import ChildAlignment, Column, Container, HorizontalAlignment, Row, Text
 from engine import Engine
-from events import Event, EventType, KeyEvent, MouseButton, MouseButtonEvent, MouseMotionEvent, QuitEvent
+from events import Event, EventType, Key, KeyEvent, MouseButton, MouseButtonEvent, MouseMotionEvent, QuitEvent
 from screens.screen import Screen
 
 
@@ -68,6 +68,45 @@ class MainMenuScreen(Screen):
             .set_child(Text("Exit", pygame.font.SysFont("Arial", 30), (0, 0, 0)))
         )
 
+        self._cancel_exit_button = (
+            Container(int(self._width * 0.075), int(self._width * 0.025))
+            .set_border((64, 128, 64))
+            .set_child(Text("cancel", pygame.font.SysFont("Arial", 20), (0, 0, 0)))
+        )
+        self._confirm_exit_button = (
+            Container(int(self._width * 0.075), int(self._width * 0.025))
+            .set_border((128, 64, 64))
+            .set_child(Text("ok", pygame.font.SysFont("Arial", 20), (0, 0, 0)))
+        )
+
+        self._exit_confirmation_popup = (
+            Container(self._width, self._height)
+            .set_background_color((0, 0, 0, 128))
+            .set_child(
+                Container(int(self._width * 0.225), int(self._width * 0.1))
+                .set_position((
+                    (self._width - int(self._width * 0.225)) // 2,
+                    (self._height - int(self._width * 0.1)) // 2)
+                )
+                .set_background_color((255, 255, 255))
+                .set_child(
+                    Column()
+                    .set_padding(15)
+                    .add_element(
+                        Text("Are you sure you want to exit?", pygame.font.SysFont("Arial", 20), (0, 0, 0))
+                    )
+                    .add_element(
+                        Row()
+                        .set_padding(15)
+                        .add_element(self._cancel_exit_button)
+                        .add_element(self._confirm_exit_button)
+                    )
+                )
+            )
+        )
+
+        self._waiting_exit_confirmation = False
+
         (column2.add_element(self._play_button)
          .add_element(self._workshop_button)
          .add_element(self._settings_button)
@@ -101,7 +140,16 @@ class MainMenuScreen(Screen):
         pass
 
     def on_key_event(self, key_event: KeyEvent) -> None:
-        pass
+        if key_event.type != EventType.KEY_DOWN:
+            return
+
+        if key_event.key == Key.ESCAPE:
+            self._waiting_exit_confirmation = not self._waiting_exit_confirmation
+            return
+
+        if key_event.key == Key.KP_ENTER or key_event.key == Key.RETURN:
+            pygame.event.post(pygame.event.Event(pygame.QUIT))
+            return
 
     def on_mouse_button_event(self, event: MouseButtonEvent) -> None:
         if event.type != EventType.MOUSE_BUTTON_DOWN or event.button != MouseButton.LEFT:
@@ -109,17 +157,35 @@ class MainMenuScreen(Screen):
 
         mouse_pos = pygame.mouse.get_pos()
 
+        if self._waiting_exit_confirmation:
+            if self._cancel_exit_button.contains(mouse_pos):
+                self._waiting_exit_confirmation = False
+                return
+
+            if self._confirm_exit_button.contains(mouse_pos):
+                pygame.event.post(pygame.event.Event(pygame.QUIT))
+                return
+
+            return
+
         if self._play_button.contains(mouse_pos):
             from screens.select_game_screen import SelectGameScreen
             self._engine.set_screen(SelectGameScreen(self._engine))
+            return
 
-        elif self._workshop_button.contains(mouse_pos):
+        if self._workshop_button.contains(mouse_pos):
             from screens.workshop_screen import WorkshopScreen
             self._engine.set_screen(WorkshopScreen(self._engine))
+            return
 
-        elif self._settings_button.contains(mouse_pos):
+        if self._settings_button.contains(mouse_pos):
             from screens.settings_screen import SettingsScreen
             self._engine.set_screen(SettingsScreen(self._engine))
+            return
+
+        if self._exit_button.contains(mouse_pos):
+            self._waiting_exit_confirmation = True
+            return
 
     def on_mouse_motion_event(self, event: MouseMotionEvent) -> None:
         pass
@@ -130,3 +196,6 @@ class MainMenuScreen(Screen):
     def render(self) -> None:
         window = pygame.display.get_surface()
         self._base.render(window)
+
+        if self._waiting_exit_confirmation:
+            self._exit_confirmation_popup.render(window)
