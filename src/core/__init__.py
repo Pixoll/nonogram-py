7ad21a749fbf8a74644e2ddf889931993b8bf4fc
Pixoll <pixoll.gamer@gmail.com>
2024-11-04 15:54:54 -0300

    Export nonogram types

--- src/core/__init__.py
+++ src/core/__init__.py
@@ -1,3 +1,4 @@
 from core.hint import Hint
-from core.nonogram import Nonogram
 from core.loader import NonogramLoader
+from core.nonogram import Nonogram
+from core.types import nonogram_matrix_t, nonogram_type_t, rgb_t
