1eb71427a9f9a71a24d67e1b126ee407985bfce8
Pixoll <pixoll.gamer@gmail.com>
2024-11-04 16:28:58 -0300

    Load user-name nonograms as well

--- src/components/row_of_nonograms.py
+++ src/components/row_of_nonograms.py
@@ -6,7 +6,7 @@ from components.element import Element
 from components.previous_info_nonogram import PreviousInfoNonogram
 from components.row import Row, VerticalAlignment
 from components.scroll_bar import ScrollBar
-from core import Nonogram, NonogramLoader
+from core import Nonogram, nonogram_type_t, NonogramLoader
 from events import Event
 
 
@@ -16,36 +16,51 @@ class RowOfNonograms(Element):
     _scrollbar: ScrollBar
     _selected_nonogram: PreviousInfoNonogram | None = None
 
-    def __init__(self, width: int, height: int):
+    def __init__(self, width: int, height: int, nonograms_type: nonogram_type_t):
         super().__init__(width, height)
         self._row = Row()
         self._list_of_nonograms = []
 
-        for i in range(1, 100):
-            nonogram = NonogramLoader.load("pre_made", i)
+        for i in range(1, 101):
+            try:
+                nonogram = NonogramLoader.load(nonograms_type, i)
+            except ValueError:
+                break
+
             self._list_of_nonograms.append(nonogram)
 
             info_nonogram = PreviousInfoNonogram(nonogram, self._height, self._height)
             self._row.add_element(info_nonogram)
+
+        if len(self._list_of_nonograms) == 0:
+            self._scrollbar = None
+            return
 
         content_width = len(self._list_of_nonograms) * self._height
         self._scrollbar = ScrollBar(width, content_width)
 
         self._row.set_alignment(VerticalAlignment.CENTER)
 
+    def deselect(self) -> None:
+        if self._selected_nonogram is not None:
+            self._selected_nonogram.set_selected(False)
+            self._selected_nonogram = None
+
     def get_selected_nonogram(self) -> Nonogram | None:
-        if self._selected_nonogram:
+        if self._selected_nonogram is not None:
             return self._selected_nonogram.get_nonogram()
         return None
 
     def set_position(self, position: tuple[int, int]) -> Self:
         self._position = position
         self._row.set_position(position)
-        self._scrollbar.set_position((position[0], position[1] + self._height - 20))
+        if self._scrollbar is not None:
+            self._scrollbar.set_position((position[0], position[1] + self._height - 20))
         return self
 
     def on_any_event(self, event: Event) -> None:
-        self._scrollbar.on_any_event(event)
+        if self._scrollbar is not None:
+            self._scrollbar.on_any_event(event)
 
         for element in self._row:
             element.on_any_event(event)
@@ -56,11 +71,13 @@ class RowOfNonograms(Element):
                 self._selected_nonogram = element
 
     def update(self):
-        self._scrollbar.update()
-        scroll_offset = self._scrollbar.x_axis
-        self._row.set_position((self._position[0] + scroll_offset, self._position[1]))
+        if self._scrollbar is not None:
+            self._scrollbar.update()
+            scroll_offset = self._scrollbar.x_axis
+            self._row.set_position((self._position[0] + scroll_offset, self._position[1]))
 
     def render(self, window: pygame.Surface) -> None:
         self.update()
         self._row.render(window)
-        self._scrollbar.render(window)
+        if self._scrollbar is not None:
+            self._scrollbar.render(window)
--- src/screens/select_game_screen.py
+++ src/screens/select_game_screen.py
@@ -9,7 +9,7 @@ from screens.screen import Screen
 
 class SelectGameScreen(Screen):
     _engine: Engine
-    _row_of_nonograms: RowOfNonograms
+    _row_of_pre_made_nonograms: RowOfNonograms
 
     def __init__(self, engine: Engine):
         self._engine = engine
@@ -44,29 +44,46 @@ class SelectGameScreen(Screen):
             .set_border((0, 0, 0, 0))
             .set_child(row1)
         )
-        self._row_of_nonograms = RowOfNonograms(self._width, int(self._height * 0.6))
+
+        self._row_of_pre_made_nonograms = RowOfNonograms(self._width, int(self._height * 0.3), "pre_made")
+        self._row_of_user_made_nonograms = RowOfNonograms(self._width, int(self._height * 0.3), "user_made")
+
         container2 = (
-            Container(self._width, self._row_of_nonograms.size[1])
+            Container(self._width, self._row_of_pre_made_nonograms.size[1])
             .set_background_color((0, 0, 0, 200))
             .set_border((0, 0, 0, 0))
-            .set_child(self._row_of_nonograms)
+            .set_child(self._row_of_pre_made_nonograms)
         )
         container3 = (
-            Container(self._width, int(self._height * 0.8) - self._row_of_nonograms.size[1])
+            Container(self._width, self._row_of_user_made_nonograms.size[1])
+            .set_background_color((0, 0, 0, 200))
+            .set_border((0, 0, 0, 0))
+            .set_child(self._row_of_user_made_nonograms)
+        )
+        container4 = (
+            Container(
+                self._width,
+                int(self._height * 0.8)
+                - self._row_of_pre_made_nonograms.size[1]
+                - self._row_of_user_made_nonograms.size[1]
+            )
             .set_background_color((0, 0, 0, 200))
             .set_border((0, 0, 0, 0))
         )
+
         self._column = (
             Column()
             .add_element(container1)
             .add_element(container2)
             .add_element(container3)
+            .add_element(container4)
         )
+
         self._base.set_child(self._column)
 
     def on_any_event(self, event: Event) -> None:
-        self._row_of_nonograms.on_any_event(event)
-        pass
+        self._row_of_pre_made_nonograms.on_any_event(event)
+        self._row_of_user_made_nonograms.on_any_event(event)
 
     def on_key_event(self, key_event: KeyEvent) -> None:
         pass
@@ -77,11 +94,19 @@ class SelectGameScreen(Screen):
 
         mouse_pos = pygame.mouse.get_pos()
 
+        if self._row_of_pre_made_nonograms.contains(mouse_pos):
+            self._row_of_user_made_nonograms.deselect()
+
+        if self._row_of_user_made_nonograms.contains(mouse_pos):
+            self._row_of_pre_made_nonograms.deselect()
+
         if self._play_button.contains(mouse_pos):
             from screens.play_screen import PlayScreen
-            selected_nonogram = self._row_of_nonograms.get_selected_nonogram()
+            selected_nonogram = self._row_of_pre_made_nonograms.get_selected_nonogram()
             if selected_nonogram:
-                from screens.play_screen import PlayScreen
+                self._engine.set_screen(PlayScreen(self._engine, selected_nonogram))
+            selected_nonogram = self._row_of_user_made_nonograms.get_selected_nonogram()
+            if selected_nonogram:
                 self._engine.set_screen(PlayScreen(self._engine, selected_nonogram))
 
         if self._back_button.contains(mouse_pos):
