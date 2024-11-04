73b41c4b21a22f0f6b269650932e1184eb239020
Pixoll <pixoll.gamer@gmail.com>
2024-11-04 17:11:56 -0300

    Add music

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import pygame
 from pygame._sdl2 import Window
 
+from assets import SoundManager
 from engine import Engine
 from screens.main_menu_screen import MainMenuScreen
 
@@ -12,6 +13,7 @@ window = pygame.display.set_mode((1280, 720), flags=pygame.RESIZABLE)
 Window.from_display_module().maximize()
 
 pygame.display.set_caption("Nonogram")
+SoundManager.play_music("music.mp3")
 
 engine = Engine(window)
 engine.set_screen(MainMenuScreen(engine))
