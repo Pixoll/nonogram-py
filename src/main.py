93b2f461c17b73d8d088612765a44c931c4880a6
Pixoll <pixoll.gamer@gmail.com>
2024-11-24 22:56:53 -0300

    Set window icon

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ import pygame
 # noinspection PyProtectedMember
 from pygame._sdl2 import Window
 
-from assets import SoundManager
+from assets import SoundManager, TextureManager
 from engine import Engine
 from screens.main_menu_screen import MainMenuScreen
 
@@ -13,6 +13,7 @@ window = pygame.display.set_mode((1280, 720), flags=pygame.RESIZABLE)
 
 Window.from_display_module().maximize()
 
+pygame.display.set_icon(TextureManager.get("icon.png"))
 pygame.display.set_caption("Nonogram")
 SoundManager.play_music("music.mp3")
 
